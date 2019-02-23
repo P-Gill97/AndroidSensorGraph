743b7bab8d4051ca10b0dca430d9ff3be509c909
P-Gill97 <pgill@csus.edu>
2019-02-23 15:11:09 -0800

    added new activity and listview to hold sensor names

--- SensorGraph/app/src/main/java/com/example/android/sensorgraph/MainActivity.java
+++ SensorGraph/app/src/main/java/com/example/android/sensorgraph/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.sensorgraph;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] sensors = {"Barometer", "Thermometer"} ;
+        ListAdapter Adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_2, sensors );
+
     }
 }
