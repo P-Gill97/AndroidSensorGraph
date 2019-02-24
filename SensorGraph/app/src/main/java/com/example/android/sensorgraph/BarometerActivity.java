29985d77281e4e6b6b0e34a85b46b0ab02cb6adb
P-Gill97 <pgill@csus.edu>
2019-02-23 19:28:25 -0800

    added barometerCode will add graphView once i figure out how it works.

--- SensorGraph/app/src/main/java/com/example/android/sensorgraph/BarometerActivity.java
+++ SensorGraph/app/src/main/java/com/example/android/sensorgraph/BarometerActivity.java
@@ -1,13 +1,51 @@
 package com.example.android.sensorgraph;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class BarometerActivity extends AppCompatActivity {
 
+    private SensorManager manager;
+    private Sensor barometerSensor;
+    private TextView tv;
+    private SensorEventListener eventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float[] values = event.values;
+            tv.setText(String.format("%.2f Millibar",values[0]));
+            // add graph dtat here.
+
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barometer);
+        tv = findViewById(R.id.barometer_text_display);
+        manager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        barometerSensor = manager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        manager.registerListener(eventListener,barometerSensor,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        manager.unregisterListener(eventListener);
     }
 }
--- SensorGraph/app/src/main/java/com/example/android/sensorgraph/MainActivity.java
+++ SensorGraph/app/src/main/java/com/example/android/sensorgraph/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.android.sensorgraph;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
+import android.widget.ListView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +16,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // string array of sensors.
         String[] sensors = {"Barometer", "Thermometer"} ;
-        ListAdapter Adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_2, sensors );
+
+        // add string of sensors to an adapter so we can use them in a listview.
+        ListAdapter sensorAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, sensors );
+        // creating a list view
+        ListView sensorListView;
+        // setting list view to the one in xml for the list view.
+        sensorListView = (ListView) findViewById(R.id.main_sensor_list);
+
+        //
+        sensorListView.setAdapter(sensorAdapter);
+
+        sensorListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i ==0){
+                    barometerIntent(view);
+                }
+                if(i ==0){
+
+                }
+            }
+        });
+    }
+    public void barometerIntent(View view){
+
+        Intent newIntent = new Intent(view.getContext(), BarometerActivity.class);
+        startActivityForResult(newIntent,0);
+
+    }
+    public void thermometerIntent(View view){
+
+        Intent newIntent = new Intent(view.getContext(), ThermometerActivity.class);
+        startActivityForResult(newIntent,0);
 
     }
 }
