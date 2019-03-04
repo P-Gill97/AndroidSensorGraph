4dd8f2ed34ad20c3ad6a4b7bc39ed188bbe7e3f7
P-Gill97 <pgill@csus.edu>
2019-03-04 13:59:23 -0800

    Not working . Graphview problem.

--- SensorGraph/app/src/main/java/com/example/android/sensorgraph/BarometerActivity.java
+++ SensorGraph/app/src/main/java/com/example/android/sensorgraph/BarometerActivity.java
@@ -1,5 +1,4 @@
 package com.example.android.sensorgraph;
-
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -7,7 +6,6 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.series.DataPoint;
@@ -18,14 +16,17 @@ public class BarometerActivity extends AppCompatActivity {
     private SensorManager manager;
     private Sensor barometerSensor;
     private TextView tv;
+    private int x = 0;
     private LineGraphSeries<DataPoint>  points;
+    GraphView graph = (GraphView) findViewById(R.id.barometer_graph);
     private SensorEventListener eventListener = new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent event) {
             float[] values = event.values;
+
             tv.setText(String.format("%.2f Millibar",values[0]));
             // add changes to graphview here
-
+            addData((int)values[0], x++ );
         }
 
         @Override
@@ -41,7 +42,7 @@ public class BarometerActivity extends AppCompatActivity {
         manager = (SensorManager) getSystemService(SENSOR_SERVICE);
         barometerSensor = manager.getDefaultSensor(Sensor.TYPE_PRESSURE);
 
-        GraphView graph = (GraphView) findViewById(R.id.barometer_graph);
+
         graph.addSeries(points);
         Viewport viewport = graph.getViewport();
         viewport.setYAxisBoundsManual(true);
@@ -50,14 +51,16 @@ public class BarometerActivity extends AppCompatActivity {
         viewport.setScrollable(true);
     }
     // add data to graph
-    private void addData(){
-        // resume here
+    private void addData(int y, int x){
+        // not try to call fun in the right class.
+    points.appendData(new DataPoint(x,y ),false, 1000);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         manager.registerListener(eventListener,barometerSensor,SensorManager.SENSOR_DELAY_NORMAL);
+
     }
 
     @Override
